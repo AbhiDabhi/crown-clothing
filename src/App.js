f6b6faac104f6965a41af539f4b188fc63f75a8b
AbhiDabhi <dabhiabhishek5601@gmail.com>
2020-10-05 19:53:59 -0400

    broken homepage into smaller components, styled menu-item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <div className="App">Hello World</div>
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
