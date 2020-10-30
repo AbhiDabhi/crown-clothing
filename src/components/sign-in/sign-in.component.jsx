d22a75459f202fd06367906fcee4daf04c452455
AbhiDabhi <dabhiabhishek5601@gmail.com>
2020-10-30 14:32:35 -0400

    Added sign-in, form, input, button components and related styles.

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import './sign-in.styles.scss';
+
+class SingIn extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.setState({ email:'', password:'' })
+    }
+
+    handleChange = event => {
+        const { value, name } = event.target;
+
+        this.setState({ [name]: value });
+    }
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form aonSubmit={this.handleSubmit}>
+                    <FormInput 
+                        type="email" 
+                        name='email' 
+                        value={this.state.email} 
+                        handleChange={this.handleChange}
+                        label='email'
+                        required />
+
+                    <FormInput 
+                        type="password" 
+                        name='password' 
+                        value={this.state.password} 
+                        handleChange={this.handleChange}
+                        label='password'
+                        required />
+
+                    <CustomButton type="submit" > Sign In </CustomButton>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SingIn;
